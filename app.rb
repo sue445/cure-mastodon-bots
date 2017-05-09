185c323c2cff9c8b0b788e886e7f07111db48a63
sue445 <sue445@sue445.net>
2017-05-10 01:21:57 +0900

    Add birthday girls

--- app.rb
+++ app.rb
@@ -12,6 +12,28 @@ class App < Sinatra::Base
   use Rollbar::Middleware::Sinatra
 
   get "/" do
+    today = Time.current.to_date
+    @date_girls = girl_birthdays(today, today + 3.months)
     slim :index
+  end
+
+  helpers do
+    def girl_birthdays(from_date, to_date)
+      date_girls = {}
+      girls = Precure.all.select(&:have_birthday?)
+
+      from_year = from_date.year
+      to_year = to_date.year
+      girls.each do |girl|
+        (from_year..to_year).each do |year|
+          date = Date.parse("#{year}/#{girl.birthday}")
+          date_girls[date] = girl
+        end
+      end
+
+      date_girls.select! { |date, _girl| (from_date..to_date).cover?(date) }
+
+      Hash[date_girls.sort]
+    end
   end
 end
